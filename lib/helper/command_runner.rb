ff23b753badf1d5d92cc5aa2eee7c1bd36e0b34c
Akash <akashm@thoughtworks.com>
2012-05-27 21:08:43 +0530

    Added test for command runner

--- lib/helper/command_runner.rb
+++ lib/helper/command_runner.rb
@@ -1,6 +1,6 @@
 module CommandRunner
   def execute(dir, command)
-    return "" if command.nil? || dir.nil?
+    return nil if command.nil? || dir.nil?
     Dir.chdir(dir)
     command  = `#{command}`
   end
---
+++ test/helper/command_runner_test.rb
@@ -0,0 +1,21 @@
+require File.join(File.dirname(__FILE__), '../..', 'lib', 'helper', 'command_runner')
+require "test/unit"
+
+include CommandRunner
+
+class CommandRunnerTest < Test::Unit::TestCase
+
+  def test_execute_return_nil_for_nil_dir
+    assert_nil execute(nil,"ls")
+  end
+
+  def test_execute_return_nil_for_nil_command
+    assert_nil execute("/",nil)
+  end
+
+   def test_execute_runs_command
+     output = `ls`
+     assert_equal output, execute(Dir.pwd,"ls")
+   end
+
+end
