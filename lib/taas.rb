cd31ecea699d08477cec9df5e9ec97da078def39
abagmar <abagmar@gmail.com>
2012-05-31 19:17:00 +0530

    Require only the server.rb - directly in the start_server method

--- lib/taas.rb
+++ lib/taas.rb
@@ -1,12 +1,6 @@
-Dir.glob('**/*.rb').each { |r|
-  puts "\t.. importing: #{r}"
-  require r
-}
-
 module TaaS
-  # Your code goes here...
   def self.start_server
-    puts "TaaS.start_server"
+    puts "Starting TaaS Server"
     server_path = "#{File.dirname(__FILE__)}/server/server.rb"
     eval("ruby server_path")
   end
