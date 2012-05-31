b15c1ef975be4b14a7ad8695f5108a23a63f0b96
abagmar <abagmar@gmail.com>
2012-05-31 16:52:19 +0530

    disable rake command. require all .rb files from taas.rb

--- lib/taas.rb
+++ lib/taas.rb
@@ -1,6 +1,14 @@
-require "taas/version"
-import "#{File.dirname(__FILE__)}/**/*.rake"
+Dir.glob('**/*.rb').each { |r|
+  puts "Importing: #{r}"
+  require r
+}
+#require "taas/version"
+
+#import "#{File.dirname(__FILE__)}/**/*.rake"
 
 module TaaS
   # Your code goes here...
+  def start_server
+    puts "TaaS.start_server"
+  end
 end
