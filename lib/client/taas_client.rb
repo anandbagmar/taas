1cddb96020eb6020b89ce77a9a098f400701c7a6
Akash <akashm@thoughtworks.com>
2012-06-06 23:23:25 +0530

    Updated Taas Client to return the hash of the contract response

--- lib/client/taas_client.rb
+++ lib/client/taas_client.rb
@@ -1,9 +1,9 @@
-module TaaS
-  require 'uri'
-  require 'net/http'
-  @taas = nil
+require 'uri'
+require 'net/http'
+require "json"
 
-  class TaaSClient
+
+class TaaSClient
 
     attr_accessor :url, :timeout
 
@@ -12,44 +12,24 @@ module TaaS
       @timeout = timeout_in_seconds * 1000
     end
 
-    def execute_contract(contract_name, input_params)
-      contract_response = {}
-      uri = URI.parse(url_for(contract_name,input_params))
-      http_end_point = Net::HTTP.new(uri.host, uri.port)
+    def execute_contract(contract_name, params={})
+      uri = URI.parse(@url)
 
-      #configure the timeout setting for the http endpoint
-      http_end_point.read_timeout = @timeout
-
-      http_response = http_end_point.request(Net::HTTP::Get.new(uri.request_uri))
-      puts "http_response for contract: #{contract_name} with parameters:\n\t#{input_params}\n"
-      puts http_response
-      contract_response[:http_response] = http_response
-      contract_response
+      response = Net::HTTP.post_form(uri, params)
+      JSON.parse(strip_body_tags(parse_output(response.body)))
     end
 
     private
 
-    def url_for(contract_name, input_params)
-      contract_url = @url + "/#{contract_name}"
-      input_params.values.each {|input_param_value| contract_url += "/#{input_param_value}" }
-      contract_url
+    def strip_body_tags(string)
+      string.gsub(/<body>/, "").gsub(/<\/body>/, "")
+    end
+
+    def parse_output(body)
+      "#{body.match(/<body>(.*)<\/body>/m)}"
     end
   end
 
-  def self.configure (url,timeout_in_seconds)
-    return nil if url.nil?
-    @taas = (@taas.nil? ? TaaSClient.new(url, timeout_in_seconds) : @taas)
-  end
 
-  def self.execute(contract_name,input_params={})
-    return nil if contract_name.nil?
-    if (@taas.nil?)
-      raise 'TaaS NOT configured. Please configure TaaS first by calling the following method:\n\tTaaS.configure(url,timeout)'
-    end
-    @taas.execute_contract(contract_name,input_params)
-  end
 
-  def self.reset
-    @taas = nil
-  end
-end
+
--- test/client/taas_client_test.rb
+++ test/client/taas_client_test.rb
@@ -1,61 +1,30 @@
+require "rubygems"
 require File.join(File.dirname(__FILE__), '../..', 'lib', 'client', 'taas_client')
 require "test/unit"
 require "yaml"
-
+require "mocha"
+require "json"
 
 class TaaSClientTest < Test::Unit::TestCase
 
-  def self.start_test_server
-    puts "Starting TaaS test Server"
-    server_path = "#{File.dirname(__FILE__)}/../server/math_service.rb"
-    eval("ruby server_path")
+  def test_execute_contract
+    input_params = {:name => "taas"}
+    output_params = {"id"=>1}
+
+    taas_client = TaaSClient.new("http://localhost:4567/")
+
+    uri = URI.parse("http://localhost:4567/")
+    URI.stubs(:parse).with("http://localhost:4567/").returns(uri)
+
+    response = "<!DOCTYPE html>\n<html>\n<head>\n  <style type=\"text/css\">\n  body { text-align:center;font-family:helvetica,arial;font-size:22px;\n    color:#888;margin:20px}\n  #c {margin:0 auto;width:500px;text-align:left}\n  </style>\n</head>\n<body>{\"id\":1}</body>\n</html>\n"
+
+
+    Net::HTTP.stubs(:post_form).with(uri, input_params).returns(Net::HTTPFound)
+    Net::HTTPFound.stubs(:body).returns(response)
+
+    assert_equal output_params, taas_client.execute_contract("http://localhost:4567/", input_params)
+
   end
 
-  def test_configure_return_nil_for_nil_url
-    assert_nil TaaS.configure(nil, 10)
-  end
 
-  def test_execute_return_nil_for_nil_contractName
-    TaaS.configure("http://localhost:4567", 10)
-    assert_nil TaaS.execute(nil, {})
-  end
-
-  #def test_execute_contract_create_live_sale
-  #  yaml = YAML.load_file("#{File.dirname(__FILE__)}/resources/test_contracts.yaml")
-  #  yamlObject = yaml.to_hash
-  #
-  #  contract_name = "create_simple_live_sale"
-  #  input_params = yamlObject["Contracts"]["Contract"]["create_simple_live_sale"]["Input_Params"]
-  #  time_out = yamlObject["Contracts"]["Contract"]["create_simple_live_sale"]["timeout_in_seconds"]
-  #
-  #  TaaS.configure("http://localhost:4567",time_out)
-  #
-  #  assert_not_nil TaaS.execute(contract_name, input_params)
-  #end
-
-  def test_add_contract_with_numbers
-    yaml = YAML.load_file("#{File.dirname(__FILE__)}/../resources/test_contracts.yaml")
-    yamlObject = yaml.to_hash
-    puts yamlObject
-
-    contract_name = "add_two_numbers"
-    input_params = yamlObject["contracts"]["add_two_numbers"]["input_params"]
-    timeout_in_seconds = yamlObject["contracts"]["add_two_numbers"]["timeout_in_seconds"]
-
-    TaaS.configure("http://localhost:4567", timeout_in_seconds)
-    assert_not_nil TaaS.execute(contract_name, input_params)
-  end
-
-  def test_add_contract_with_strings
-    yaml = YAML.load_file("#{File.dirname(__FILE__)}/../resources/test_contracts.yaml")
-    yamlObject = yaml.to_hash
-    puts yamlObject
-
-    contract_name = "add_two_numbers"
-    input_params = yamlObject["contracts"]["add_two_invalid_numbers"]["input_params"]
-    timeout_in_seconds = yamlObject["contracts"]["add_two_invalid_numbers"]["timeout_in_seconds"]
-
-    TaaS.configure("http://localhost:4567", timeout_in_seconds)
-    assert_not_nil TaaS.execute(contract_name, input_params)
-  end
 end
