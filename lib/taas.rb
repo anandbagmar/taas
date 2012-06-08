be1a0bd1bb2862ecbb0674d7a5b2a2c68ce200be
Akash <akashm@thoughtworks.com>
2012-06-08 10:57:10 +0530

    Adding contract file argument to taas start

--- lib/taas.rb
+++ lib/taas.rb
@@ -1,10 +1,10 @@
 require File.expand_path("#{File.dirname(__FILE__)}/client/taas_client")
 
 module TaaS
-  def self.start_server
-    puts "Starting TaaS Server"
-    server_path = "#{File.dirname(__FILE__)}/server/server.rb"
-    eval("ruby server_path")
+  def self.start_server(contract_file)
+    puts "Starting TaaS Server with Contract File #{contract_file}"
+    server_path = "#{File.dirname(__FILE__)}/server/server.rb #{contract_file}"
+    `ruby #{server_path}`
   end
 
   def self.client(url,timeout)
