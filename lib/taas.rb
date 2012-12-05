43ec19a27b730ada6bcd3bf8fbc9c28f5c570a0b
Akash Mishra <akash.mishra20@gmail.com>
2012-12-05 12:16:18 +0530

    Changed the taas.rb file to reflect the refactored changes

--- lib/taas.rb
+++ lib/taas.rb
@@ -1,15 +1,15 @@
 require "server"
 require "taas_client"
+require "helper/command_executer"
+require "helper/contracts"
 
-module Taas
-  class << self
+module TaaS
     def self.start_server(contract_file)
       Contracts.load(contract_file)
-      CommandExecuter.execute_command("ruby server.rb", File.dirname(__FILE__))
+      CommandExecuter.execute_contract(File.dirname(__FILE__),"ruby server.rb")
     end
 
     def self.client(url, timeout)
       TaaSClient.new(url, timeout)
     end
-  end
 end
