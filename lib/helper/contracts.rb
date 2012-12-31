ea8129723d1d1493030e2ec56d665779c57789c5
Anand Bagmar <abagmar@gmail.com>
2012-12-31 16:50:00 +0530

    Anand - raise an exception if the absolute_path is empty or nil

--- lib/helper/contracts.rb
+++ lib/helper/contracts.rb
@@ -1,10 +1,11 @@
+require 'pry'
 module TaaS
   class Contracts
 
     @@contract_hash = {}
 
     def self.load(absolute_path)
-      raise "File path cant be nil or empty" if absolute_path.empty?
+      raise "File path cant be nil or empty" if (absolute_path.nil? || absolute_path.empty?)
       @@contract_hash = YAML.load_file(absolute_path)["contracts"] rescue {}
     end
 
